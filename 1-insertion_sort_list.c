b2b0679ee5320b9bd27e10f8d583c242ccbe6bb9
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-10-10 18:56:11 +0200

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,23 +2,37 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *previous;
-	
+	listint_t *spot, *current, *last;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
 	current = (*list)->next;
-	previous = (*list)->prev;
+	last = *list;
 
-	while (current < previous && current != NULL)
+	for (; current; current = current->next)
 	{
-		
-		previous = current->prev;
-
-		while (current < previous)
+		last = current;
+		while (last && last->prev && last->n < last->prev->n)
 		{
-			previous->prev = previous->prev->next;
+			spot = last->prev;
+			if (spot->prev)
+			{
+				spot->prev->next = last;
+			}
+			else
+			{
+				*list = last;
+			}
+			if (last->next)
+			{
+				last->next->prev  = spot;
+			}
+			spot->next = last->next;
+			last->prev = spot->prev;
+			last->next = spot;
+			spot->prev = last;
+			print_list(*list);
 		}
-
-		current->prev = previous->prev->next;
-		current->next = previous->prev;
-		previous->next = previous->next->prev;
 	}
 }
