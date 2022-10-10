aba6730c173a876a05c6359591fbcff63750f380
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-10-10 18:30:26 +0200

    sorting_algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,13 +25,11 @@ void selection_sort(int *array, size_t size)
 			{
 				min = j;
 			}
-			
 		}
-		
 		temp = array[i];
 		array[i] = array[min];
 		array[min] = temp;
 
 		print_array(array, size);
 	}
-}	
+}
